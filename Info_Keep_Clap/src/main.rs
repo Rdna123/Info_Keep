2dae0d0e756a55645e2a10a7aa1d5f655bb4ad1f
Robert Kimura <chknmanrob1904@gmail.com>
2021-08-23 00:39:03 -0700

    first commit

---
+++ Info_Keep_Clap/src/main.rs
@@ -0,0 +1,138 @@
+fn main() {
+    use Info_Keep_Lib::{export_db, import_db, new_entry, search_tag, sort_db, Tag};
+    use chrono::prelude::*;
+    use clap::{crate_version, App, Arg, SubCommand};
+    use read_input::InputBuild;
+
+    let matches = App::new("Info Keep")
+        .version(crate_version!())
+        .author("Robert Kimura <chknmanrob1904@gmail.com>")
+        .about("Stores information you want to keep using date-time format")
+        .arg(
+            Arg::with_name("year")
+                .short("y")
+                .long("year")
+                .takes_value(true)
+                .help("year for new entry or search")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("month")
+                .short("m")
+                .long("month")
+                .takes_value(true)
+                .help("month for new entry or search")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("Day")
+                .short("d")
+                .long("day")
+                .takes_value(true)
+                .help("Day for new entry or search")
+                .takes_value(true),
+        )
+        .subcommand(
+            SubCommand::with_name("new")
+                .about("Adds info to keep: Use parentheses")
+                .help("Adds info to keep: info_keep.exe [OPTIONS] new \"<INFO>\"")
+                .alias("n")
+                .arg(
+                    Arg::with_name("INFO")
+                        .help("Information you wish to keep")
+                        .takes_value(true)
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("search")
+                .about("Search kept info, not options for full listing")
+                .help("Searches info kept: info_keep.exe [OPTIONS] search")
+                .alias("s"),
+        )
+        .subcommand(
+            SubCommand::with_name("delete")
+                .about("Deletes a key given date and time key, can use options")
+                .alias("d")
+                .arg(
+                    Arg::with_name("KEY")
+                        .help("Key to delete info: 0000-00-00+00:00:00")
+                        .takes_value(true),
+                ),
+        )
+        .subcommand(SubCommand::with_name("clear").about("Used to clear info keep"))
+        .subcommand(
+            SubCommand::with_name("export")
+                .about("Exports the Info Keep data to transfer data\n Creates ik_Export.txt")
+                .alias("e"),
+        )
+        .subcommand(
+            SubCommand::with_name("import")
+                .about("Imports Info Keep data\nReads ik_Export.txt")
+                .alias("i"),
+        )
+        .get_matches();
+
+    let open_db: sled::Db = sled::open("Dates").expect("Error opening Data base");
+
+    let sort = match matches.subcommand().0.is_empty() {
+        true => {
+            println!("{}", matches.usage());
+            false
+        }
+        false => true,
+    };
+
+    let (mut db, _) = sort_db(open_db, sort);
+
+    let key: String;
+    let tag: Tag;
+    if matches.is_present("year") || matches.is_present("month") || matches.is_present("Day") {
+        let year = matches.value_of("year");
+        let month = matches.value_of("month");
+        let day = matches.value_of("Day");
+        let time = Utc::now().format("+%H:%M:%S").to_string();
+        tag = Tag::new(year, month, day);
+        key = Tag::new(year, month, day).full_tag() + &*time;
+    } else {
+        tag = Tag::new(None, None, None);
+        key = Utc::now().format("%Y-%m-%d+%H:%M:%S").to_string();
+    };
+    // let key = String::from("Test");
+
+    if let Some(matches) = matches.subcommand_matches("new") {
+        let info = matches.value_of("INFO").unwrap();
+        db = new_entry(db, &key, info.to_string())
+    }
+    if matches.subcommand_matches("search").is_some() {
+        search_tag(&db, tag);
+    }
+
+    if matches.subcommand_matches("import").is_some() {
+        db = import_db(db, None);
+    }
+
+    if matches.subcommand_matches("export").is_some() {
+        export_db(&db);
+    }
+
+    if matches.subcommand_matches("clear").is_some() {
+        let input = read_input::prelude::input::<bool>()
+            .msg("Are you sure (true/False): ")
+            .default(false);
+        if input.get() {
+            db.clear().expect("Could not clear Info Keep data");
+        }
+    };
+
+    #[cfg(not(debug_assertions))]
+    if matches.subcommand_matches("delete").is_some() {
+        if matches.is_present("KEY") {
+            db.remove(matches.value_of("KEY").unwrap().as_bytes())
+                .expect("Could not remove key");
+        }
+    }
+
+    #[cfg(debug_assertions)]
+    db.remove(key.as_bytes()).expect("Key does not exist");
+}
---
+++ Info_Keep_Iced/src/main.rs
@@ -0,0 +1,223 @@
+use Info_Keep_Lib::{export_db, import_db, new_entry, search_tag, sort_db, Tag};
+use chrono::prelude::*;
+
+use iced::{
+    button, pick_list, scrollable, text_input, Align, Button, Container, Element, Length, PickList,
+    Sandbox, Scrollable, Settings, Space, Text, TextInput,
+};
+use std::ops::Deref;
+
+pub fn main() {
+    InfoKeep::run(Settings::default()).expect("Could not open GUI");
+}
+
+#[derive(Default)]
+struct InfoKeep {
+    scroll: scrollable::State,
+    text_box: text_input::State,
+    mode_section: pick_list::State<Options>,
+    input_data: Option<String>,
+    selected_mode: Option<Options>,
+    confirm_button: button::State,
+    output: Option<String>,
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    InputData(String),
+    ModeSection(Options),
+    ConfirmButton,
+    // Output
+}
+
+impl Sandbox for InfoKeep {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("Info Keep")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ModeSection(option) => self.selected_mode = Some(option),
+            Message::InputData(text) => self.input_data = Some(text),
+            Message::ConfirmButton => {
+                run_command(
+                    self.selected_mode.expect("No mode selected"),
+                    self.input_data.clone(),
+                );
+                self.input_data = Some(String::new());
+            }
+            // Message::Output => self.output = Some(output_db())
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        let pick_list = PickList::new(
+            &mut self.mode_section,
+            &Options::ALL[..],
+            self.selected_mode,
+            Message::ModeSection,
+        );
+
+        let place_holder = if self.selected_mode == Some(Options::DeleteEntry) {
+            "Entry key for deletion"
+        } else {
+            "Input data to keep"
+        };
+
+        let value = match self.input_data.as_ref() {
+            None => String::new(),
+            Some(text) => text.to_string(),
+        };
+
+        let output = match self.output.as_ref() {
+            None => output_db(),
+            Some(text) => {
+                if self.selected_mode != Some(Options::SearchDB) {
+                    text.to_string()
+                } else {
+                    run_command(Options::SearchDB, self.input_data.clone())
+                }
+            }
+        };
+
+        println!("{}", &output);
+
+        let input_box = TextInput::new(
+            &mut self.text_box,
+            place_holder,
+            &*value,
+            Message::InputData,
+        )
+        .padding(10)
+        .width(Length::FillPortion(10));
+
+        let button_confirm = Button::new(&mut self.confirm_button, Text::new("Confirm"))
+            .on_press(Message::ConfirmButton);
+
+        let mut content = Scrollable::new(&mut self.scroll)
+            .width(Length::Fill)
+            .align_items(Align::Center)
+            .spacing(10)
+            .push(Space::with_height(Length::Units(5)))
+            .push(Text::new("Select Mode"))
+            .push(pick_list)
+            .push(input_box)
+            .push(button_confirm)
+            .push(Text::new(output));
+
+        content = content.push(Space::with_height(Length::Units(600)));
+
+        Container::new(content)
+            .width(Length::Fill)
+            .height(Length::Fill)
+            .center_x()
+            .center_y()
+            .into()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Options {
+    NewEntry,
+    ExportDB,
+    ImportDB,
+    DeleteEntry,
+    SearchDB,
+}
+
+impl Options {
+    const ALL: [Options; 5] = [
+        Options::NewEntry,
+        Options::ExportDB,
+        Options::ImportDB,
+        Options::DeleteEntry,
+        Options::SearchDB,
+    ];
+}
+
+impl Default for Options {
+    fn default() -> Options {
+        Options::NewEntry
+    }
+}
+
+impl std::fmt::Display for Options {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Options::NewEntry => "New Entry",
+                Options::ExportDB => "Export Database",
+                Options::ImportDB => "Import Database",
+                Options::DeleteEntry => "Delete Entry",
+                Options::SearchDB => "Search DB",
+            }
+        )
+    }
+}
+
+fn output_db() -> String {
+    let db = sled::open("Dates").unwrap();
+    let (_, keys) = sort_db(db, false);
+    let mut outputs: Vec<String> = Vec::new();
+    for (k, v) in keys {
+        outputs.push(format!(
+            "{} :: {}\n",
+            String::from_utf8_lossy(&*k),
+            String::from_utf8_lossy(&*v)
+        ))
+    }
+    let mut mid = String::new();
+    for output in outputs {
+        mid += output.as_str()
+    }
+    mid
+}
+
+fn run_command(mode: Options, data: Option<String>) -> String {
+    let open_db: sled::Db = sled::open("Dates").expect("Error opening Data base");
+    let (db, _) = sort_db(open_db, false);
+    let key = Utc::now().format("%Y-%m-%d+%H:%M:%S").to_string();
+    let mut output = String::new();
+    match mode {
+        Options::NewEntry => {
+            new_entry(db, &key, data.unwrap());
+        }
+        Options::ExportDB => {
+            export_db(&db);
+        }
+        Options::ImportDB => {
+            import_db(db, None);
+        }
+        Options::DeleteEntry => {
+            db.remove(data.unwrap().as_bytes())
+                .expect("Could not remove key");
+        }
+        Options::SearchDB => {
+            let mid: Vec<String> = {
+                let tmp = data.unwrap();
+                tmp.split("+").map(|s| s.to_string()).collect()
+            };
+
+            let dates: Vec<String> = {
+                let tmp: Vec<&str> = mid.get(0).unwrap().split("-").collect();
+                tmp.iter().map(|s| s.deref().deref().to_string()).collect()
+            };
+
+            let tag = Tag::new(
+                dates.get(0).map(|s| s.as_str()),
+                dates.get(1).map(|s| s.as_str()),
+                dates.get(2).map(|s| s.as_str()),
+            );
+            output = search_tag(&db, tag);
+        }
+    }
+    output
+}
