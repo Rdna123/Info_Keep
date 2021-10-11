ddbc801d2c530d4a4189dd85d0dd1dbd7bc042e6
Robert Kimura <chknmanrob1904@gmail.com>
2021-10-10 18:31:48 -0700

    Removed commented out code

--- info_keep_lib/src/lib.rs
+++ info_keep_lib/src/lib.rs
@@ -24,9 +24,6 @@ impl InfoKeep {
     }
 
     pub fn sort_db(&mut self, sort: bool) -> Vec<(sled::IVec, sled::IVec)> {
-        // use std::thread;
-
-       // let thread = thread::spawn(move || {
         let keys_iter = self.db.iter();
         let mut bh_keys = BinaryHeap::new();
 
@@ -47,9 +44,6 @@ impl InfoKeep {
             }
         }
         keys
-        // });
-        // let result = thread.join();
-        // result.unwrap()
     }
 
     pub fn export_db(&mut self) {
@@ -241,149 +235,3 @@ impl Tag {
         full_tag
     }
 }
-
-
-// pub fn new_entry(db: Database::Db, key: &str, info: String) -> Database::Db {
-//     db.insert(key.as_bytes(), info.as_bytes())
-//         .expect("Could not enter value");
-//     #[cfg(not(feature = "iced"))]
-//     {
-//         println!("\nInfo added for {}", &key);
-//         let mid = db.get(&key.as_bytes()).unwrap().unwrap();
-//         let result = String::from_utf8_lossy(&*mid);
-//         println!("{}\n", result);
-//     }
-//     db
-// }
-//
-// pub fn export_db(db: &Database::Db) {
-//     let mut file = match std::fs::File::create("ik_Export.txt") {
-//         Ok(file) => file,
-//         Err(_) => {
-//             std::fs::remove_file("ik_Export.txt").expect("Could not create file");
-//             std::fs::File::create("ik_Export.txt").expect("Could not create file")
-//         }
-//     };
-//     let (_, kvs) = sort_db(db.clone(), false);
-//     file.write_all("info_keep_file\n".as_bytes())
-//         .expect("Could not write to export file");
-//     for (k, v) in kvs {
-//         let key = String::from_utf8_lossy(&*k);
-//         let value = String::from_utf8_lossy(&*v);
-//         file.write_all((key + "=" + value + "\n").as_bytes())
-//             .expect("Could not write to fill");
-//     }
-// }
-//
-// pub fn import_db(db: Database::Db, import_file: Option<&Vec<(String, String)>>) -> Database::Db {
-//     let file: std::fs::File = if import_file.is_none() {
-//         std::fs::File::open("ik_Export.txt").expect("No such file exists")
-//     } else {
-//         let mut file = std::fs::File::create("ik_Export.txt").expect("Could not make data file");
-//         file.write_all(b"Database_file\n")
-//             .expect("Could not write to import file");
-//         for (k, v) in import_file.unwrap() {
-//             let key = String::from_utf8_lossy(k.as_ref());
-//             let value = String::from_utf8_lossy(v.as_ref());
-//             file.write_all((key + "=" + value + "\n").as_bytes())
-//                 .expect("Could not write to fill");
-//         }
-//         file
-//     };
-//     let buf = std::io::BufReader::new(file);
-//     let mut lines: Vec<String> = buf
-//         .lines()
-//         .map(|l| l.expect("could not read file"))
-//         .collect();
-//     if lines.contains(&"info_keep_file".to_string()) {
-//         lines.remove(0);
-//         for key_value in lines {
-//             let mid_kv: Vec<&str> = key_value.split('=').collect();
-//             let k = mid_kv.get(0).unwrap();
-//             let v = mid_kv.get(1).unwrap();
-//
-//             let key = k.to_string();
-//             let value = v.to_string();
-//             db.insert(key.as_bytes(), value.as_bytes())
-//                 .expect("Could not import data");
-//         }
-//     }
-//     print_db(&db);
-//     db
-// }
-//
-// pub fn search_tag(db: &Database::Db, tag: Tag) -> String {
-//     let full_tag = tag.full_tag();
-//
-//     let mut output = String::new();
-//
-//     if full_tag != BEGINNING_DATE {
-//         output = format!("Searching for entries with tag: {}\n", full_tag).to_string()
-//     } else {
-//         print_db(db);
-//         return "Listing all entries".to_string();
-//     };
-//
-//     let iter: Database::Iter = db.range((full_tag.as_bytes())..);
-//
-//     for i in iter.flatten() {
-//         let (k, v) = i;
-//
-//         output += &*format!(
-//             "{} :: {} \n",
-//             String::from_utf8_lossy(&*k),
-//             String::from_utf8_lossy(&*v)
-//         )
-//     }
-//
-//     output
-// }
-//
-// pub fn print_db(db: &Database::Db) {
-//     let (_, keys) = sort_db(db.clone(), false);
-//     for (k, v) in keys {
-//         println!(
-//             "{} :: {}",
-//             String::from_utf8_lossy(&*k),
-//             String::from_utf8_lossy(&*v)
-//         );
-//     }
-// }
-//
-// pub fn sort_db(
-//     db: Database::Db,
-//     sort: bool,
-// ) -> (Database::Db, Vec<(Database::IVec, Database::IVec)>) {
-//     use std::thread;
-//     let thread = thread::spawn(move || {
-//         let keys_iter = db.iter();
-//         let mut bh_keys = BinaryHeap::new();
-//
-//         for k in keys_iter {
-//             bh_keys.push(k.unwrap());
-//         }
-//
-//         let mut keys = bh_keys.into_sorted_vec();
-//
-//         if sort {
-//             keys.dedup();
-//             #[cfg(not(feature = "iced"))]
-//             let time = std::time::Instant::now();
-//
-//             #[cfg(not(feature = "iced"))]
-//             println!("###Sorting Keys###");
-//
-//             db.clear().expect("Could not clear Database of sorting");
-//             for (k, v) in keys.clone() {
-//                 db.insert(k, v).expect("Could not enter value");
-//             }
-//
-//             #[cfg(not(feature = "iced"))]
-//             println!("DONE: {}s\n", time.elapsed().as_secs());
-//         }
-//
-//         (db, keys)
-//     });
-//     let result = thread.join();
-//     result.unwrap()
-// }
